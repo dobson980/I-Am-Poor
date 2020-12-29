b7312de01ae81031e285e86b95d09c9f33feb5d9
Thomas Dobson <Dobson980@gmail.com>
2020-12-28 23:55:07 -0800

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Thomas Dobson on 12/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
